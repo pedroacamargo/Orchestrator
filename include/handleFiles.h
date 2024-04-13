1d3c3b8155adfff13b05d2222e095d3cb15d499d
bernazyzz <zebernardomoniz@gmail.com>
2024-04-13 23:48:36 +0100

    remove-process working

--- include/handleFiles.h
+++ include/handleFiles.h
@@ -5,7 +5,6 @@
 
 void writeProcess(Process p, int fd, int command_id);
 int removeProcessFromFile(const char *filename, int command_id);
-int handleFiles(char queue[][100], int i, ProcessStatus status);
 int handleProcess(Process p);
 int countLines(const char *filename);
 
